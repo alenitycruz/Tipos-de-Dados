853bb9fbfc277328e7705528e354b89a22969869
alenitycruz <alessandradsmcruz@recife.pe.senac.br>
2022-02-12 18:42:54 -0300

    Exercicio de aplicacao de tipos de dados

---
+++ src/br/com/exercicio/Main.java
@@ -0,0 +1,58 @@
+package br.com.exercicio;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = -10000000000;
+        int i2 = 40000;
+
+        long l1 = 10000000033000000L;
+        long l2 = 20000055000000000L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.04d;
+
+
+        char c1 = 'M';
+        //char c2 = 'Tm';
+        char c3 = '\u0057';
+
+        String st1 = "Fulano";
+        String st2 = "Cicrano";
+        String st3 = "Beltrano é filho de Cicrano e neto de Fulano";
+
+        //String st4 = "27/08/1987";
+
+        Boolean bo1 = true;
+        Boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+
+    }
+}
